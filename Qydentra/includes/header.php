6bccfe9dc8292f338df07863474cf2ebbfcecb49
zyreen <bethany.zyreenamosco4@gmail.com>
2026-06-04 20:11:02 +0800

    w/ receptionist

---
+++ Qydentra/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Qydentra</title>
+
+<link rel="stylesheet" href="../assets/css/dashboard.css">
+
+<!-- GOOGLE FONT -->
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+<!-- FONT AWESOME -->
+
+<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+</head>
