05c172977f2407477526978eb375d901139b2617
zyreen <bethany.zyreenamosco4@gmail.com>
2026-06-06 10:46:19 +0800

    Update pass.php

--- sql/pass.php
+++ sql/pass.php
@@ -1,3 +1,5 @@
 <?php
-echo password_hash("receptionist123", PASSWORD_BCRYPT);
+echo password_hash("receptionist123", PASSWORD_BCRYPT);   // for receptionist
+echo password_hash('qydentra.admin', PASSWORD_DEFAULT);   // for admin
+echo password_hash('qydentra.dentist', PASSWORD_DEFAULT); // for dentist
 ?>
